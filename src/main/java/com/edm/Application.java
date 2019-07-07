950b5d1a9f7594c527d42c7a0656b9b8d2f27280
xieshang <xieshang728@163.com>
2019-07-08 07:27:31 +0800

    add json util

--- src/main/java/com/edm/Application.java
+++ src/main/java/com/edm/Application.java
@@ -3,15 +3,16 @@ package com.edm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @author xieshang728
  */
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @Configuration
-@MapperScan(basePackages = "com.edm.mapper")
+//@MapperScan(basePackages = "com.edm.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
